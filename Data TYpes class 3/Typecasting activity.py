fa4cf74e76b95e7035cf12012322211828bebe51
parinitimodi <Shaktikansalmodi@gmail.com>
2024-11-12 17:44:32 +0530

    initial commit

---
+++ Data TYpes class 3/Typecasting activity.py
@@ -0,0 +1,14 @@
+Name = "Pariniti"
+Age = 11
+Weight = 34.5
+Good = True 
+print ("type of name",type(Name))
+print ("type of age",type(Age))
+print ("type of weight",type(Weight))
+print ("type of good",type(Good))
+# after typecasting.....
+print("AFTER TYPECASTING...")
+Age = str(Age)
+print ("type of age",type(Name))
+Weight = int(Weight)
+print ("type of weight",type(Weight))
