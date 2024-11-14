a4236d66716112381cf93bbd137e6320f26de2b9
parinitimodi <Shaktikansalmodi@gmail.com>
2024-11-14 17:50:33 +0530

    initial commit

---
+++ Operators 1/Marks percentage.py
@@ -0,0 +1,10 @@
+print ("Enter marks obtained in 3 subjects:")
+Math = int(input("Maths:"))
+English = int(input("English:"))
+Science = int(input("Science:"))
+#Lets calculate the percentage of marks obtained!!
+Sum = Math + English + Science
+print("sum of marks",Sum)
+Percentage = (Sum/300)*100
+print(end = "Percentage marks =")
+print (Percentage)
