fec5fa3aa3cc88000e6f84cd51d0360d2a619343
parinitimodi <Shaktikansalmodi@gmail.com>
2024-11-14 17:19:23 +0530

    initial commit

---
+++ Operators 1/Average.py
@@ -0,0 +1,10 @@
+Tree1 = 80
+Tree2 = 34
+Tree3 = 96
+Tree4 = 50
+Tree5 = 30
+Sum = Tree1 + Tree2 + Tree3 + Tree4 + Tree5
+print ("The sum of all trees is", Sum)
+#Finding the average
+print ("To find the average, we divide the sum by the number of constituents that is", Sum/5)
+print ("We found the average!!")
