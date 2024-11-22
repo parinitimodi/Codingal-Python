3a0a79376d7322010f5a0d88a59a0a815de518bb
parinitimodi <Shaktikansalmodi@gmail.com>
2024-11-22 19:11:24 +0530

    initial commit

---
+++ Conditional Statements class 5/Homework.py
@@ -0,0 +1,6 @@
+print ("'Oh i do not know what to wear according to the appropriate seasons!! Can u please help me' said Ram")
+Clothes1 = "Light cotton clothes"
+Clothes2 = "Dark woolen clothes" 
+if ("It is summer "): print (Clothes1)
+else: print (Clothes2)
+print ("'Yay, thank yiu so much, i now know what to wear according to the appropriate season!! <3")
