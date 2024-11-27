3829c9afdfb8484c101d1f9507d6d720cde90b21
parinitimodi <Shaktikansalmodi@gmail.com>
2024-11-27 15:24:00 +0530

    initial commit

---
+++ Python operators II class 6/AND - OR OPERATOR.py
@@ -0,0 +1,22 @@
+a = 28
+b = 24
+c = 0
+
+
+if a and b and c:
+    print("All the numbers are having boolean value true!!")
+else:
+    print("Atleast one number has a boolean value as false!!")
+   
+
+
+   #Sub activity 2 
+a = 23
+b = 45
+c = 19
+if a>b or a<c:
+    print ("Im using or operator")
+elif a<b and a>c:
+    print("Im using and operator")
+else:
+    print("Im using another operator")
---
+++ Python operators II class 6/NOT OPERATOR.py
@@ -0,0 +1,24 @@
+a = 100
+b = 140
+c = 140
+print(a!=b)
+print (b!=c)
+
+
+a = "PYTHON"
+b = "CODINGAL"
+if a!=b:
+    print(a,"and",b, "are different")
+
+
+a = 7
+b = 8
+if (a==1)!=(b==8):
+    print("HELLO")
+
+
+a = int(input("Enter a number"))
+if a%2!=0:
+    print ("The number is odd!!")
+else:
+    print("The number is even")
