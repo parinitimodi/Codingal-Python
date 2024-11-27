1249f7c9d7736dba8fdd7337de65b078d67e5e90
parinitimodi <Shaktikansalmodi@gmail.com>
2024-11-27 15:47:42 +0530

    initial commit

---
+++ Python operators II class 6/HOMEWORK.py
@@ -0,0 +1,6 @@
+print ("I WANT TO CHECK IF THIS USER IS FILLED WITH LETTERS OR NUMBERS!!")
+if ord(str (input("ENTER A CHARACTER"))):
+    print("ITS AN ALPHABET!!")
+else:
+    print("ITS NOT AN ALPHABET")
+
