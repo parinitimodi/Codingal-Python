7eafe8d5a12a17f8b4a0e7afd57b570cc2386644
parinitimodi <Shaktikansalmodi@gmail.com>
2024-11-28 17:34:33 +0530

    initial commit

---
+++ PYTHON OPERATORS III/BITWISE OPERATOR.py
@@ -0,0 +1,10 @@
+a = 5
+b = 10
+# right shifting a from one gives us??
+print (a>>1 )
+# left shifting a from one gives us??
+print (a<<1)
+if (a<<1 == b):
+    print ("TRUE")
+else:
+    print("FALSE")
