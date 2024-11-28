2a6228166d08399650e663fdca93f1cdeb9fa3c6
parinitimodi <Shaktikansalmodi@gmail.com>
2024-11-28 17:45:14 +0530

    initial commit

---
+++ PYTHON OPERATORS III/GRADING.py
@@ -0,0 +1,18 @@
+
+marks1 = 97
+marks2 = 80
+marks3= 100
+marks4 = 68
+marks5 = 72
+sum = (marks1+marks2+marks3+marks4+marks5)
+average = sum/5
+if (average>=91 and average<=100):
+    print("A2")
+elif (average>=81 and average<=90):
+    print("B2")
+elif (average>=71 and average<=80):
+    print("C2")
+elif (average>=61 and average<=70):
+    print("D2")
+else:
+    print("E2")
