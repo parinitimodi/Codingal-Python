063b8093a14966e9a3a49b038cb39cfedba93a5e
parinitimodi <Shaktikansalmodi@gmail.com>
2024-12-03 17:32:37 +0530

    initial commit

---
+++ REVISION CLASS/CHECKING MEAN.py
@@ -0,0 +1,13 @@
+average1 = 38
+wrongnumber = 36
+correctnumber = 56
+totalnumber = 40
+
+sum = average1*totalnumber
+print (sum)
+
+sum2 = sum - wrongnumber + correctnumber
+
+Mean2 = sum2/totalnumber
+print (Mean2)
+
