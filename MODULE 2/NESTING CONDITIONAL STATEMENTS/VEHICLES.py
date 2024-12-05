f9bf278eeeaaf27ec37c3c5f84c4e59e4d3034f9
parinitimodi <Shaktikansalmodi@gmail.com>
2024-12-05 17:50:19 +0530

    initial commit

---
+++ MODULE 2/NESTING CONDITIONAL STATEMENTS/VEHICLES.py
@@ -0,0 +1,21 @@
+value = input("ENTER USER INPUT EITHER BIKE OR CAR")
+if (value == "bike"):
+    print("THERE ARE TWO TYPES OF BIKES MOTOR AND SCOOTY")
+    value1 = input("CHOOSE")
+    if value1 =="motor":
+        print ("SO MOTOR IT IS !!")
+    else:
+        print("SO SCOOTY IT IS!!")
+else:
+    print("THERE ARE TWO TYPES OF CARS SUV OR SEDAN")
+    value2 = input("CHOOSE")
+    if value2 =="suv":
+        print("SUV IT ISS!!")
+    else:
+        print("SEDAN IT IS!!")
+        
+
+    
+
+
+
