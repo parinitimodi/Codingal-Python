889ba786a556fa36bf61afa977ae91d34368339a
parinitimodi <Shaktikansalmodi@gmail.com>
2024-12-07 11:56:45 +0530

    initial commit

--- Conditional Statements class 5/Homework.py
+++ Conditional Statements class 5/Homework.py
@@ -1,6 +1,14 @@
-print ("'Oh i do not know what to wear according to the appropriate seasons!! Can u please help me' said Ram")
+print ("'Oh i do not know what to wear according to the appropriate seasons!! Can u please help me' said John")
 Clothes1 = "Light cotton clothes"
 Clothes2 = "Dark woolen clothes" 
-if ("It is summer "): print (Clothes1)
-else: print (Clothes2)
-print ("'Yay, thank yiu so much, i now know what to wear according to the appropriate season!! <3")
+Clothes3 = "Mild cool clothes"
+Clothes4 = "Sweatshirts and Jackets to keep warm"
+season = str(input("ENTER USER INPUT EITHER PUT SUMMER, WINTER, AUTUMN OR SPRING"))
+if (season == "summer"):
+    print (Clothes1)
+elif (season == "winter"):
+    print(Clothes2)
+elif(season == "spring"):
+    print(Clothes3)
+else:
+    print(Clothes4)
