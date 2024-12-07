da5ed9f2cad5298435b9d1e1d7de80beead8eb3f
parinitimodi <Shaktikansalmodi@gmail.com>
2024-12-07 11:47:38 +0530

    INITIAL COMMIT

---
+++ MODULE 2/NESTING CONDITIONAL STATEMENTS/HOMEWORK.py
@@ -0,0 +1,15 @@
+age = int(input("ENTER USER INPUT"))
+if (age<5 and age>0):
+    print("YOU ARE A BABY")
+elif (age>5 and age<10):
+    print("YOU ARE A CHILD")
+elif(age>10 and age<20):
+    print("YOU ARE A TEENAGER")
+elif (age>20 and age<40):
+    print("YOU ARE AN ADULT")
+elif(age>40 and age<55):
+    print("YOU ARE IN MIDDLE AGE")
+else:
+    print("YOU ARE OLD")
+
+
