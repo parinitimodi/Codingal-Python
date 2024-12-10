72b0efc0fefa801f0531183d8822a1484dacec13
parinitimodi <Shaktikansalmodi@gmail.com>
2024-12-10 17:40:27 +0530

    initial commit

---
+++ MODULE 2/LOOPS/ACTIVITY1.py
@@ -0,0 +1,5 @@
+number = int(input("ENTER USER INPUT:"))
+sum = 0
+for i in range(1,number):
+    sum += i 
+    print("sum = :", sum)
---
+++ MODULE 2/LOOPS/ACTIVITY2.py
@@ -0,0 +1,8 @@
+string = input("ENTER STRING!!!")
+string2 = ("")
+
+for i in string:
+    string2 = i +string2
+
+print("\n The Original String",string) 
+print("The reversed string =", string2)
