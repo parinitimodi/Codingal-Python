fabac1b7a06093505c606e56fd01a62b0488b642
parinitimodi <Shaktikansalmodi@gmail.com>
2024-12-12 17:43:13 +0530

    initial commit

---
+++ MODULE 2/WHILE LOOP.py/ARMSTRONG.py
@@ -0,0 +1,10 @@
+
+
+a = int(input("ENTER"))
+b = int(input("ENTER"))
+c = int(input("ENTER"))
+number = (a*100 + b*10 + c*1)
+if (a*a*a + b*b*b + c*c*c == number):
+    print("TRUE")
+else:
+    print("FALSE")
