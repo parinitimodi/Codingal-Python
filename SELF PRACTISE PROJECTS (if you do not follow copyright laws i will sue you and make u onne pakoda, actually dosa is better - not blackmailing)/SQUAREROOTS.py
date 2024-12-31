33e271b5bad48ae94a3cb9337c7bf068da3332dc
parinitimodi <Shaktikansalmodi@gmail.com>
2024-12-31 09:51:23 +0530

    initial commit

---
+++ SELF PRACTISE PROJECTS (if you do not follow copyright laws i will sue you and make u onne pakoda, actually dosa is better - not blackmailing)/SQUAREROOTS.py
@@ -0,0 +1,8 @@
+targetnumber = int(input("ENTER NUMBER: "))
+whattypeofroot = int(input("ENTER ROOT: "))
+
+answer = int(targetnumber ^ (1/whattypeofroot))
+
+print (answer)
+
+
---
+++ SELF PRACTISE PROJECTS (if you do not follow copyright laws i will sue you and make u onne pakoda, actually dosa is better - not blackmailing)/STORING BIRTHDAYS.py
@@ -0,0 +1,6 @@
+numberofbirthdays = input("ENTER NUMBER OF BIRTHDAYS: ")
+birthday1 = input("ENTER 1ST BIRTHDAY: ")
+birthday2 = input("ENTER 2ND BIRTHDAY: ")
+birthday3 = input("ENTER 3RD BIRTHDAY: ")
+
+print("THE NUMBER OF BIRTHDAYS YOU HAVE STORED ARE",numberofbirthdays,"WHICH ARE",birthday1,",",birthday2,"AND",birthday3,".")
