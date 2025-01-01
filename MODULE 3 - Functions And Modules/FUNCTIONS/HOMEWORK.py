8afe59d0aadd4f7fe2593c886d707f25646e3b9e
parinitimodi <Shaktikansalmodi@gmail.com>
2025-01-01 10:50:50 +0530

    initial commit

--- MODULE 3 - Functions And Modules/FUNCTIONS/HOMEWORK.py
+++ MODULE 3 - Functions And Modules/FUNCTIONS/HOMEWORK.py
@@ -3,4 +3,4 @@ import math
 radius = float(input("ENTER THE RADIUS OF THE CIRCLE: "))
 CIRCUMFERENCE = 2*math.pi*radius
 
-print("The Circumference of the circle is ",CIRCUMFERENCE)
+print("The Circumference of the circle is ",CIRCUMFERENCE, ".")
