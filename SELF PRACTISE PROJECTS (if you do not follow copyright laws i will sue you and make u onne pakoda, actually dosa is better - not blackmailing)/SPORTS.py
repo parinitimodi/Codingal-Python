eb9953b3b2fe7e64c4fcf1a38cd813bfc62c9db6
parinitimodi <Shaktikansalmodi@gmail.com>
2025-01-06 18:43:13 +0530

    initial commit

---
+++ SELF PRACTISE PROJECTS (if you do not follow copyright laws i will sue you and make u onne pakoda, actually dosa is better - not blackmailing)/SPORTS.py
@@ -0,0 +1,26 @@
+print("This program is to tell that which friend of mine is good in which sport:  ")
+
+friend = input("Enter a friends name, either Pari, Shriparna, Anushka, Pratham, Vedant, Atharva, Tanmay, Priyanshi or Yashashvi  or Saurabh: ")
+
+if (friend == "pari"):
+    print("SHE IS THE BEST AT BASKETBALL AND FOOTBALL AND PLAYS GOOD BADMINTON")
+elif (friend == "shriparna"):
+    print("She is good at basketball and badminton" )
+elif (friend == "anushka"):
+    print("She is good at badminton" )
+elif (friend == "priyanshi"):
+    print("She is good at singing" )
+elif (friend == "yashashvi"):
+    print("She is good at basketball and kind of ok in badminton" )
+elif (friend == "tanmay"):
+    print("He is good at basketball" )
+elif (friend == "pratham"):
+    print("HE IS THE BEST AT FOOTBALL, BASKETBALL AND CAN PLAY BADMINTON A BIT" )
+elif (friend == "atharva or korean"):
+    print("HE IS THE BEST AT FOOTBALL AND BADMINTON" )
+elif (friend == "vedant"):
+    print("He is good at tennis" )
+elif (friend == "saurabh"):
+    print("HE IS THE BEST AT NOT TAKING A BATH FOR A WHOLE MONTH" )
+else:
+    print("ERROR! Name not identified in system")
