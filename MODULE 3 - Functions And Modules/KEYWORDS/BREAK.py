f918ffb45a8fba76ee523023257ad9ff0becaef2
parinitimodi <Shaktikansalmodi@gmail.com>
2025-01-07 17:18:42 +0530

    initial commit

---
+++ MODULE 3 - Functions And Modules/KEYWORDS/BREAK.py
@@ -0,0 +1,21 @@
+a = input("ENTER A WORD ")
+
+var = a.lower()
+
+for i in var:
+    if (i == "i"):
+        print("i, is found")
+
+        break
+
+    else:
+        print("i, is not found")
+    
+    
+
+
+
+    
+
+
+    
