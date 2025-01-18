783ce2171e9ba56b7786c42448a352cedb838b48
parinitimodi <Shaktikansalmodi@gmail.com>
2025-01-18 10:21:05 +0530

    initial commit

---
+++ MODULE 3 - Functions And Modules/DATE, TIME AND CALENDER MODULE/Date and Time NOW.py
@@ -0,0 +1,12 @@
+from datetime import date,time,datetime
+
+now = datetime.now()
+
+today = date.today()
+
+
+print("The Date today is ",today)
+print("The Time Today is ",now)
+print(today.year,"\n")
+print(today.month,"\n")
+print(today.day,"\n")
---
+++ SELF PRACTISE PROJECTS (if you do not follow copyright laws i will sue you and make u onne pakoda, actually dosa is better - not blackmailing)/TESTING.py
@@ -0,0 +1,6 @@
+import random
+value =  (random.randint(0,11))
+print (value)
+list = [0,1,2,3,4,5,6,7,8,9,10]
+value2 = (random.sample(list,10)) 
+print (value2)
