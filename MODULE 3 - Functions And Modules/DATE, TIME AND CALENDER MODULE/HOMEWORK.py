801308d3b39d138bbdb1295872a7a837226be8c1
parinitimodi <Shaktikansalmodi@gmail.com>
2025-01-20 15:19:13 +0530

    initial commit

---
+++ MODULE 3 - Functions And Modules/DATE, TIME AND CALENDER MODULE/HOMEWORK.py
@@ -0,0 +1,5 @@
+import calendar
+
+listofmonths = list(calendar.month_name)[1:]
+
+print(listofmonths)
---
+++ MODULE 3 - Functions And Modules/RANDOM AND MATH MODULES/HOMEWORK.py
@@ -0,0 +1,8 @@
+import math
+print (math.sin(65))
+print (math.cos(65))
+print (math.tan(65))
+
+print (math.ceil(65.490))
+print (math.floor(65.890))
+print (math.factorial(6))
