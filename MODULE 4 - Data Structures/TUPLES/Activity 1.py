e7703da0f7314b553a5ac143846b2f215a44c271
parinitimodi <Shaktikansalmodi@gmail.com>
2025-01-23 17:31:23 +0530

    initial commit

---
+++ MODULE 4 - Data Structures/TUPLES/Activity 1.py
@@ -0,0 +1,18 @@
+tuplex = ("tuple",False,3.2,1)
+print(tuplex)
+
+tuplex = (2,3,4,6,8)
+print(tuplex)
+
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuple1 = (50,20,70,50)
+print(tuple1.count(50))
+
+tuplex = (2,4,3,5,6,7,7,6)
+slice = tuplex[3:5]
+
+slice = tuplex[:6]
+print(slice)
+
