9613c8de12cc3d537408246a461fcb7b9d0d72dc
parinitimodi <Shaktikansalmodi@gmail.com>
2025-01-26 10:24:54 +0530

    initial commit

--- MODULE 3 - Functions And Modules/KEYWORDS/Homework.py
+++ MODULE 3 - Functions And Modules/KEYWORDS/Homework.py
@@ -1,12 +1,14 @@
+
 x = int(input("Please enter bill amount "))
-y = int(input("Please enter the amount you paid"))
+y = int(input("Please enter the amount you paid "))
 def calculate(x,y):
     if (y>x):
         return("Change =",{y - x})
     
     elif (x>y):
-        return
+        return ("Due Amount",{x - y})
     
     else:
-    print("There is no amount left to pay")
+        return("There is no amount left to pay")
+    
 
--- MODULE 4 - Data Structures/TUPLES/Homework.py
+++ MODULE 4 - Data Structures/TUPLES/Homework.py
@@ -1,37 +1,14 @@
 def mutiple_tuple(nums):
-    
     temp = list(nums)
-    
-    product = 1 
-    
+    product = 1
     for x in temp:
-    
         product *= x
-    
-    
     return product
-
-
 nums = (4, 3, 2, 2, -2, 18)
-
-
 print ("Original Tuple: ")
-
 print(nums)
-
-
 print("Product - multiplying all the numbers of the said tuple:", mutiple_tuple(nums))
-
-
 nums = (2, 4, 8, 8, 3, 2, 9)
-
 print ("\nOriginal Tuple: ")
-
 print(nums)
-
 print("Product - multiplying all the numbers of the said tuple:", mutiple_tuple(nums)) 
-
-
-
-
-
