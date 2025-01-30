88cf9f3c0bf10a8b6932575ba442edd9dfd143f5
parinitimodi <Shaktikansalmodi@gmail.com>
2025-01-30 17:33:47 +0530

    initial commit

---
+++ MODULE 4 - Data Structures/SETS AND ARRAYS/ACTIVITY 2.py
@@ -0,0 +1,8 @@
+setx = {"green","blue"}
+sety = {"yellow","blue"}
+print("Original set elements: ")
+print(setx)
+print(sety)
+print("In Intersection Of 2 Said Sets: ")
+setz = setx.intersection(sety)
+print(setz)
