db7153606d9d96c1f7abfe28aa7e8d9a6dbff47b
parinitimodi <Shaktikansalmodi@gmail.com>
2025-01-30 17:26:32 +0530

    initial commit

---
+++ MODULE 4 - Data Structures/SETS AND ARRAYS/ACTIVITY 1.py
@@ -0,0 +1,12 @@
+myset = {1,2,3}
+print(myset)
+myset = {1.0,"Hello",(1,2,3)}
+print(myset)
+myset = {1,2,3,4,3,2}
+print(myset,"\n")
+numset = set([0,1,3,4,5])
+print("Original Set: ")
+print(numset)
+numset.pop()
+print("After removing the first element from the said set: ")
+print(numset,"\n")
