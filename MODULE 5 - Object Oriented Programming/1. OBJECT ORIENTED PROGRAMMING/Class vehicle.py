977daaf6c8e5a94f116273a8d53d0631ec41f4ea
parinitimodi <Shaktikansalmodi@gmail.com>
2025-02-11 17:30:22 +0530

    initial commit

---
+++ MODULE 5 - Object Oriented Programming/1. OBJECT ORIENTED PROGRAMMING/Class vehicle.py
@@ -0,0 +1,12 @@
+class Vehicle:
+    def __init__(self,maxspeed,milleage):
+    
+        self.maxspeed = maxspeed
+        self.milleage = milleage
+  
+modelX = Vehicle(240,18)
+print("Model Max speed :",modelX.maxspeed)
+print("Model Milleage :",modelX.milleage)
+
+
+
