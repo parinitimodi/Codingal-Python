1cc54fc6b54b4e360edbd14da951f7dd6971b4dd
parinitimodi <Shaktikansalmodi@gmail.com>
2025-02-15 09:45:47 +0530

    initial commit

--- MODULE 4 - Data Structures/GETTING STARTED WITH LISTS/HOMEWORK.py
+++ MODULE 4 - Data Structures/GETTING STARTED WITH LISTS/HOMEWORK.py
@@ -3,8 +3,9 @@ a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 evens = [n for n in a if n % 2 == 0]
 odds = [n for n in a if n % 2 != 0]
 
-print(evens)  
-print(odds)
+print(a)
+print("Even numbers from the list are",evens)  
+print("Odd numbers from the list are",odds)
 
 res = [i * i for i in a if i % 2 != 0]
-print(res)
+print("Square of the elements =", res)
--- MODULE 5 - Object Oriented Programming/1. OBJECT ORIENTED PROGRAMMING/HOMEWORK.py
+++ MODULE 5 - Object Oriented Programming/1. OBJECT ORIENTED PROGRAMMING/HOMEWORK.py
@@ -1,20 +1,16 @@
-class Dog: 
-    'A simple dog model.'
-
-    def __init__(self, name, age): 
-        'Construct name and age attributes for an instance of Dog'
-        self.name = name.title()
+print("I have 2 dogs, Murphy and Cocoa.")
+class Dogs:
+    species = "Golden Retriever"
+    otherspecies = "Shih Tzu"
+    def __init__(self,name,age,gender):
+        self.name = name
         self.age = age
-
-    def sit(self):
-        'Simulate a dog sitting in response to a command.'
-        print(self.name + " is now sitting.")
-
-    def roll_over(self):
-        'Simulate rolling over in response to a command.'
-        print(self.name + " rolled over!")
-
-my_dog = Dog(name='willie', age=6)
-
-print("My dog's name is " + my_dog.name + ".")
-print("My dog is " + str(my_dog.age) + " years old.")
+        self.gender = gender
+Murphy = Dogs ("Murphy",9,"Male")
+Cocoa = Dogs ("Cocoa",8,"Female")
+print("Murphy is a {}".format(Murphy.species))
+print("He is {} years old".format(Murphy.age))
+print(" Murphy is a",(Murphy.gender))
+print("Cocoa is a {}".format(Cocoa.otherspecies))
+print("She is {} years old".format(Cocoa.age))
+print("Cocoa is a",(Cocoa.gender))
