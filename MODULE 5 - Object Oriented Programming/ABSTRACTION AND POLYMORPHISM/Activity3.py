373a4ddb0465b6303465a0f9b397163c630e92e9
parinitimodi <Shaktikansalmodi@gmail.com>
2025-02-25 17:50:11 +0530

    initial commit

---
+++ MODULE 5 - Object Oriented Programming/ABSTRACTION AND POLYMORPHISM/Activity3.py
@@ -0,0 +1,17 @@
+class India:
+    def capital(self):
+        print("New Delhi is the capital of India")
+    def language(self):
+        print("Hindi is the most commonly spoken language in India")
+class USA:
+    def capital(self):
+        print("Washington D.C is the modern capital of USA")
+    def language(self):
+        print("English and French are the most commonly spoken language in USA")
+R = India()
+K = USA()
+R.capital()
+R.language()
+K.capital()
+K.language()
+
