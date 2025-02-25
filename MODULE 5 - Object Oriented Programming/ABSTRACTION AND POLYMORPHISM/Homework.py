45c8d101ec8e1ece126949130be2cb3f8e08e2b1
parinitimodi <Shaktikansalmodi@gmail.com>
2025-02-25 17:56:36 +0530

    initial commit

---
+++ MODULE 5 - Object Oriented Programming/ABSTRACTION AND POLYMORPHISM/Homework.py
@@ -0,0 +1,17 @@
+class Ferrari:
+    def fuel(self):
+        print("Petrol")
+    def Maxspeed(self):
+        print("Max speed is 217 mph")
+class BMW:
+    def fuel(self):
+        print("Diesel")
+    def Maxspeed(self):
+        print("Max speed is 190 mph")
+R = Ferrari()
+K = BMW()
+R.fuel()
+R.Maxspeed()
+K.fuel()
+K.Maxspeed()
+
